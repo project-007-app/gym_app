3307c816e32079f2de9f8c3fe8f26f5b09e5a2c7
project-007-app <sandeepcs601@gmail. com>
2020-12-13 12:06:35 +0530

    new

--- App.js
+++ App.js
@@ -2,44 +2,28 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Card, Title, Paragraph } from 'react-native-paper';
+import { Appbar } from 'react-native-paper';
 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Card>
-    <Card.Content>
-      <Title>Card title</Title>
-      <Paragraph>Card content</Paragraph>
-    </Card.Content>
-  </Card>
+      <Appbar.Header>
+     
+      <Appbar.Content title="GYM" />
+      
+    </Appbar.Header>
+       
+       <Card>
+         <Text>HI</Text>
+        </Card>
+      
       <StatusBar style="auto" />
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height:100,
-    width:100,
-    alignItems:'center'
-  },
-  paragraph:{
-    padding: 30,
-      backgroundColor: '#FFFFFF',
-      borderRadius: 15,
-      borderWidth:3,
-      shadowColor: 'rgba(0,0,0,0.5)',
-      shadowOffset: {
-        width: 0,
-        height: 1
-      },
-      shadowOpacity:0.5,
-    },
-  
+ 
 });
