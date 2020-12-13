d7757d2bf46e302b349f48bc1268e38cb83d62ce
project-007-app <sandeepcs601@gmail. com>
2020-12-13 21:38:32 +0530

    added progress bar

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,33 +1,51 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
-import Card from '../screens/Card';
+
+import { Card } from 'react-native-elements';
+import ProgressCircle from 'react-native-progress-circle'
+
+
+
+
+
+
+
 
 const HomeScreen = ()=>
 {
-    return <View>
-         <View style={styles.outerContainer}>
-      <ScrollView>
-       <Card>
+    return (<View>
+        <Card style={styles.card}>
+        <ProgressCircle
+            percent={30}
+            radius={50}
+            borderWidth={8}
+            color="#3399FF"
+            shadowColor="#999"
+            bgColor="#fff"
+        >
+            <Text style={{ fontSize: 20 ,color:"#3399FF"}}>{'30%'}</Text>
+        </ProgressCircle>
+        <Text> </Text>
+        </Card>
+        <Card style={styles.card}>
          <Text>HI</Text>
         </Card>
-        <Card>
-         <Text>HI</Text>
-        </Card>
-        <Card>
-         <Text>HI</Text>
-        </Card>
-        <Card>
-         <Text>HI</Text>
-        </Card>
-        </ScrollView>
+        
       </View>
       
       
-    </View>
+  
     
-};
+    )};
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    card:{
+        margin:30,
+        height:100,
+        width:"80%",
+    },
+
+});
 
 export default HomeScreen;
